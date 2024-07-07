3927660b4051c34fd2dd96bcf2fbd07e46d056d8
mohith <mohithb987@gmail.com>
2024-07-06 20:55:41 -0400

    base version - identify driver among the detected faces

--- test.py
+++ test.py
@@ -1,46 +1,50 @@
 import cv2
 import os
 
-
 cascade_path = 'haarcascade_frontalface_default.xml'
 
 if not os.path.exists(cascade_path):
     raise FileNotFoundError(f"Haar cascade XML file not found at: {cascade_path}")
 
-input_video_path = 'data/video.mp4'
-output_video_path = 'output/video_with_faces.mp4'
+input_directory = 'images/'
+output_directory = 'output_haar/'
 
-cap = cv2.VideoCapture(input_video_path)
-
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(output_video_path, fourcc, 20.0, (frame_width, frame_height))
+os.makedirs(output_directory, exist_ok=True)
 
 face_cascade = cv2.CascadeClassifier(cascade_path)
 
-def detect_and_draw_bounding_box(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+def detect_and_draw_driver_face(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(20, 20))
+    
+    largest_face_area = 0
+    driver_face = None
+    
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        print('Face detected')
+        if w * h > largest_face_area:
+            largest_face_area = w * h
+            driver_face = (x, y, w, h)
+    
+    if driver_face:
+        x, y, w, h = driver_face
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    return frame
+    return image
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+for filename in os.listdir(input_directory):
+    image_path = os.path.join(input_directory, filename)
+    output_path = os.path.join(output_directory, f'detected_{filename}')
+    print(f'detected_{filename}')
+    
+    image = cv2.imread(image_path)
+    if image is None:
+        continue
+    
+    image_with_driver_face = detect_and_draw_driver_face(image)
+    cv2.imwrite(output_path, image_with_driver_face)
 
-    frame_with_faces = detect_and_draw_bounding_box(frame)
-    cv2.imshow('Video with Detected Faces', frame_with_faces)
-    out.write(frame_with_faces)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-cap.release()
-out.release()
+    cv2.imshow('Image with Driver\'s Face', image_with_driver_face)
+    cv2.waitKey(0)
 
 cv2.destroyAllWindows()
