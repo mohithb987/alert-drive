77e9bedd8725a2289c533a7f9786d70931b1847a
mohith <mohithb987@gmail.com>
2024-07-06 21:03:06 -0400

    updated to identify driver and shotgun passenger

--- test.py
+++ test.py
@@ -1,50 +1,68 @@
 import cv2
 import os
-
-cascade_path = 'haarcascade_frontalface_default.xml'
-
-if not os.path.exists(cascade_path):
-    raise FileNotFoundError(f"Haar cascade XML file not found at: {cascade_path}")
+from mtcnn import MTCNN
 
 input_directory = 'images/'
-output_directory = 'output_haar/'
+output_directory = 'output_face_wheel/'
 
 os.makedirs(output_directory, exist_ok=True)
 
-face_cascade = cv2.CascadeClassifier(cascade_path)
+detector = MTCNN()
 
-def detect_and_draw_driver_face(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(20, 20))
-    
-    largest_face_area = 0
-    driver_face = None
-    
-    for (x, y, w, h) in faces:
-        print('Face detected')
-        if w * h > largest_face_area:
-            largest_face_area = w * h
-            driver_face = (x, y, w, h)
-    
-    if driver_face:
-        x, y, w, h = driver_face
-        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+def detect_and_draw_faces(image):
+    faces = detector.detect_faces(image)
+    faces_sorted = sorted(faces, key=lambda x: x['box'][2] * x['box'][3], reverse=True) # sort based on area occupied by the face
+
+    for i, face in enumerate(faces_sorted):
+        (x, y, w, h) = face['box']
+        half_image_width = image.shape[1] // 2
+        if x < half_image_width:
+            orange_box_start_x = 0 
+            orange_box_end_x = half_image_width
+        else:
+            orange_box_start_x = half_image_width
+            orange_box_end_x = image.shape[1]
+
+        if i == 0:  # Face with largest area (could be the driver)
+            color = (0, 255, 0) 
+            label = 'Driver?' 
+            cv2.rectangle(image, (x, y), (x+w, y+h), color, 2)
+            cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+            
+            # capture area below the face to perform steering wheel detection 
+            orange_box_start_y = y + h
+            orange_box_end_y = image.shape[0]
+            
+            cv2.rectangle(image, (orange_box_start_x, orange_box_start_y), (orange_box_end_x, orange_box_end_y), (0, 165, 255), 2)
+
+        elif i == 1:  # Second largest face (could be the shotgun passenger)
+            color = (255, 0, 0)
+            label = 'Shotgun Passenger?'
+            cv2.rectangle(image, (x, y), (x+w, y+h), color, 2)
+            cv2.putText(image, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
+
+            # capture area below the face to perform steering wheel detection
+            orange_box_start_y = y + h
+            orange_box_end_y = image.shape[0]
+            
+            cv2.rectangle(image, (orange_box_start_x, orange_box_start_y), (orange_box_end_x, orange_box_end_y), (0, 165, 255), 2)
+
+        else:
+            break
 
     return image
 
 for filename in os.listdir(input_directory):
     image_path = os.path.join(input_directory, filename)
     output_path = os.path.join(output_directory, f'detected_{filename}')
-    print(f'detected_{filename}')
     
     image = cv2.imread(image_path)
     if image is None:
         continue
     
-    image_with_driver_face = detect_and_draw_driver_face(image)
-    cv2.imwrite(output_path, image_with_driver_face)
-
-    cv2.imshow('Image with Driver\'s Face', image_with_driver_face)
+    image_with_bounding_boxes = detect_and_draw_faces(image)
+    cv2.imwrite(output_path, image_with_bounding_boxes)
+    cv2.imshow('Image with Detected Faces and Bounding Boxes', image_with_bounding_boxes)
     cv2.waitKey(0)
 
 cv2.destroyAllWindows()
